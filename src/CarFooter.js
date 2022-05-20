e346949ccfe373b5d5e6d8f97d813728f1865089
CARLY19 <carlypau384@gmail.com>
2022-05-20 13:25:24 -0400

    third component

--- src/CarFooter.js
+++ src/CarFooter.js
@@ -2,7 +2,7 @@ const CardFooter = () => {
     return (
         <div class="card_footer">
             <p>Mis Actualizaciones</p>
-            <p>14/07/2021</p>
+            <p>03/08/2020</p>
         </div>
     );
 };
---
+++ src/CardSection.js
@@ -0,0 +1,9 @@
+const CardSection = () => {
+    return (
+        <div class="card_section">
+            <p>Descripcion de la card</p>
+        </div>
+    );
+};
+
+export default CardSection;
