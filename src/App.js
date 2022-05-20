58e767b9c64e5f5e2f33c3baa5cd4dc8072051c5
CARLY19 <carlypau384@gmail.com>
2022-05-20 13:38:11 -0400

    card realizado

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   
   return (
     <div className="App">
-      {arrEstilos.map( estilo => <Card color={estilo} )}
+     {arrEstilos.map( estilo => (<Card color={estilo} />) )}
     </div>
   );
 }
